b1f007d9a0d5720239e67f8e1cb5b9e1f901ae40
mgcarbonell <mgcarbonell@gmail.com>
2021-09-14 13:54:57 -0700

    adds jonas solution in a comment

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -210,3 +210,13 @@ const calcTip = (bills) => {
 }
 
 console.log(calcTip(bills))
+
+/* JONAS SOLUTION
+const calcTip = function(bill) {
+  return bill >= && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills =[ 125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+console.log(bills, tips);
+*/
