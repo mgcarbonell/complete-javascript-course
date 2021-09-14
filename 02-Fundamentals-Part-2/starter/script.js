7ab53fff101390edf008e138d0659d0a4026ebac
mgcarbonell <mgcarbonell@gmail.com>
2021-09-13 20:06:54 -0700

    adds jonas' solution

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -140,7 +140,7 @@ const avgKoalas2 = calcAvg(koalaData2);
 
 const checkWinner = (avgDolphins, avgKoalas) => {
   if (avgDolphins >= (avgKoalas * 2)) {
-    console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas})`)
+    console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas})`);
   } else if (avgKoalas >= (avgDolphins * 2)) {
     console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins})`);
   } else {
@@ -148,12 +148,23 @@ const checkWinner = (avgDolphins, avgKoalas) => {
   }
 }
 
-console.log(checkWinner(avgDolphins1, avgKoalas1))
-console.log(checkWinner(avgDolphins2, avgKoalas2))
+console.log(checkWinner(avgDolphins1, avgKoalas1));
+console.log(checkWinner(avgDolphins2, avgKoalas2));
 
 /* Jonas solution
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
 const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
 
+const checkWinner = function(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas})`)
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins})`)
+  } else {
+    console.log('No team wins...');
+  }
+}
+}
 */
